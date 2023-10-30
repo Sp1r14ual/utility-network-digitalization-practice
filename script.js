88d7a41e72562b142cc9008ea0fbe2cbf92b6252
sp1r14ual <sp1r14ual@gmail.com>
2023-10-30 12:34:29 +0700

    feat: added popups for objects on map

--- script.js
+++ script.js
@@ -20,3 +20,7 @@ var polygon = L.polygon([
   [51.503, -0.06],
   [51.51, -0.047],
 ]).addTo(map);
+
+marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
+circle.bindPopup('I am a circle.');
+polygon.bindPopup('I am a polygon.');
